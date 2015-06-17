d1f44edb9153625dbc592b11039421069b286c94
Jonathan Robinson <jayrowe86@gmail.com>
2015-06-16 23:33:33 -0500

    created homepage view

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<head>
+    <meta charset="UTF-8">
+    <title>Fliir</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+</head>
+
+<body>
+
+@include('layouts.partials.nav')
+
+<div class="container">
+
+    @yield('content')
+</div>
+
+<script src="//code.jquery.com/jquery.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+@yield('javascript')
+</body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="top-section">
+        <div class="col-md-6">
+            <div class="top-section-title">
+                <h1>
+                    Welcome to Fliir
+                </h1>
+                <p>The best Place to find what you were looking for</p>
+            </div>
+            <img src="http://placehold.it/550x315" alt="" class="img-responsive"/>
+        </div>
+        <div class="col-md-6">
+            <div>
+                <h2>Test</h2>
+                <p>Vivamus gravida tempus odio eu iaculis. Etiam malesuada nisi nibh, quis pretium nisi elementum imperdiet. Suspendisse vel convallis est. Sed et libero et augue egestas porta. Nam accumsan ante libero, ac suscipit lacus rhoncus nec. Sed hendrerit interdum dui, scelerisque aliquet dolor ullamcorper vel. Integer elit leo, posuere sit amet sapien ac, varius dapibus mi. Phasellus facilisis mattis congue. Quisque vitae sapien a magna viverra pharetra a nec risus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed faucibus vulputate elit sit amet sollicitudin. Etiam sagittis turpis vitae augue semper vestibulum. Aenean ac tristique orci, nec hendrerit massa. Ut viverra placerat sem, ut luctus ante tincidunt ut</p>
+            </div>
+        </div>
+    </div>
+    <div></div>
+@stop
