6aeb503db5ef72a11eb43a862d4d64bfde24d4f6
Jonathan Robinson <jayrowe86@gmail.com>
2015-07-26 15:25:36 -0500

    added classes to css view

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="top-section">
+    <div class="top-section row">
         <div class="col-md-6">
             <div class="top-section-title">
                 <h1>
@@ -18,5 +18,48 @@
             </div>
         </div>
     </div>
-    <div></div>
+    <div class="highlights row">
+        <div class="col-md-4">
+            <div class="media">
+                <div class="media-left">
+                    <a href="#">
+                        <img class="media-object" src="http://placehold.it/50x50" alt="...">
+                    </a>
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading">Media heading</h4>
+                    <p>Vivamus gravida tempus odio eu iaculis. Etiam malesuada nisi nibh, quis pretium nisi elementum imperdiet. Suspendisse vel convallis est. Sed et libero et augue egestas porta. Nam accumsan ante libero, ac suscipit lacus rhoncus nec. Sed hendrerit interdum dui, scelerisque aliquet dolor ullamcorper vel. Integer elit leo, posuere sit amet sapien ac, varius dapibus mi. Phasellus facilisis mattis congue. Quisque vitae sapien a magna viverra pharetra a nec risus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed faucibus vulputate elit sit amet sollicitudin. Etiam sagittis turpis vitae augue semper vestibulum. Aenean ac tristique orci, nec hendrerit massa. Ut viverra placerat sem, ut luctus ante tincidunt ut</p>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="media">
+                <div class="media-left">
+                    <a href="#">
+                        <img class="media-object" src="http://placehold.it/50x50" alt="...">
+                    </a>
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading">Media heading</h4>
+                    <p>Vivamus gravida tempus odio eu iaculis. Etiam malesuada nisi nibh, quis pretium nisi elementum imperdiet. Suspendisse vel convallis est. Sed et libero et augue egestas porta. Nam accumsan ante libero, ac suscipit lacus rhoncus nec. Sed hendrerit interdum dui, scelerisque aliquet dolor ullamcorper vel. Integer elit leo, posuere sit amet sapien ac, varius dapibus mi. Phasellus facilisis mattis congue. Quisque vitae sapien a magna viverra pharetra a nec risus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed faucibus vulputate elit sit amet sollicitudin. Etiam sagittis turpis vitae augue semper vestibulum. Aenean ac tristique orci, nec hendrerit massa. Ut viverra placerat sem, ut luctus ante tincidunt ut</p>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="media">
+                <div class="media-left">
+                    <a href="#">
+                        <img class="media-object" src="http://placehold.it/50x50" alt="...">
+                    </a>
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading">Media heading</h4>
+                    <p>Vivamus gravida tempus odio eu iaculis. Etiam malesuada nisi nibh, quis pretium nisi elementum imperdiet. Suspendisse vel convallis est. Sed et libero et augue egestas porta. Nam accumsan ante libero, ac suscipit lacus rhoncus nec. Sed hendrerit interdum dui, scelerisque aliquet dolor ullamcorper vel. Integer elit leo, posuere sit amet sapien ac, varius dapibus mi. Phasellus facilisis mattis congue. Quisque vitae sapien a magna viverra pharetra a nec risus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed faucibus vulputate elit sit amet sollicitudin. Etiam sagittis turpis vitae augue semper vestibulum. Aenean ac tristique orci, nec hendrerit massa. Ut viverra placerat sem, ut luctus ante tincidunt ut</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
