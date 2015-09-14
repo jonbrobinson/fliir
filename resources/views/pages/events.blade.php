bf64d7782a3c5744508d78c135840360593364af
Jonathan Robinson <jayrowe86@gmail.com>
2015-09-13 21:31:11 -0500

    rewrite view for events page

--- resources/views/pages/events.blade.php
+++ resources/views/pages/events.blade.php
@@ -6,8 +6,19 @@
         <h1>All Events</h1>
     </div>
     <div>
-        @foreach($output['events'] as $event)
-            <h2>Name: <?= $event['name']['text']?></h2>
+
+        @foreach( $output['events'] as $event)
+            <div class="media">
+                <div class="media-left">
+                    <a href="<?= $event['url'];?>" >
+                        <img class="media-object img-responsive" src="<?= $event['logo']['url'];?>" alt="logo">
+                    </a>
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading"><?= $event['name']['text']?> </h4>
+                    <p><?= $event['description']['text'];?></p>
+                </div>
+            </div>
         @endforeach
     </div>
 @stop
