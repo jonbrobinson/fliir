87cdc2659d107c7a6c29e17aa8574915d8103aaa
Jonathan Robinson <jayrowe86@gmail.com>
2015-10-25 23:48:21 -0500

    added font awesome icons

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,6 +7,8 @@
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/app.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     @yield('head')
 </head>
 
