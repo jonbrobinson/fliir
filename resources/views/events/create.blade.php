12f062fa2f464888e226bb20a565ba117d8d9649
Jonathan Robinson <jayrowe86@gmail.com>
2015-11-01 23:05:33 -0600

    added image input for events form

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -35,9 +35,15 @@
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
                     <h2>What</h2>
-                    <div class="form-group">
-                        <label for="title">Title: </label>
-                        <input type="text" id="title" name="title" class="form-control" value="{{ old('title') }}">
+                    <div class="row">
+                        <div class="form-group col-md-9">
+                            <label for="title">Title: </label>
+                            <input type="text" id="title" name="title" class="form-control" value="{{ old('title') }}">
+                        </div>
+                        <div class="form-group col-md-3">
+                            <label for="photo">Flyer: </label>
+                            <input type="file" title=" " id="photo" name="photo" class="form-control" value="{{ old('photo') }}">
+                        </div>
                     </div>
 
                     <div class="form-group">
